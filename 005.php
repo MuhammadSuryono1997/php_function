3391958b3ad8e6af7838078646cbcbf88aa46c96
Muhammad Suryono <msuryono0@gmail.com>
2020-07-07 15:10:47 +0700

    PHP Basic Challenge

--- 005.php
+++ 005.php
@@ -1,11 +1,15 @@
 <?php
     $csv = array_map('str_getcsv', file("file.csv"));
-    
     array_walk($csv, function(&$data) use ($csv) 
     {
       $data = array_combine(array_map('strtolower',$csv[0]), $data);
     });
     array_shift($csv);
+    uasort($csv, function($a, $b){
+    	return $a['price'] - $b['price'];
+    });
+
+
     print_r($csv);
 ?> 
 
