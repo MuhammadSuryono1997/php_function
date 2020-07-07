76c203c61ed5d4f5e649d313ba7c7f92e7560f2b
Muhammad Suryono <msuryono0@gmail.com>
2020-07-07 14:59:43 +0700

    PHP Basic Challenge

--- 005.php
+++ 005.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
+    $csv = array_map('str_getcsv', file("file.csv"));
+    
+    array_walk($csv, function(&$data) use ($csv) 
+    {
+      $data = array_combine(array_map('strtolower',$csv[0]), $data);
+    });
+    array_shift($csv);
+    print_r($csv);
+?> 
 
-	$data = fopen("file.csv", "r");
-	echo $data;
- ?>
+
