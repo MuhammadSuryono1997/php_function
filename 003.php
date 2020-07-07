e998a87ccd7e5551e7a0f62d0248b3e6d13e9e15
Muhammad Suryono <msuryono0@gmail.com>
2020-07-07 13:57:12 +0700

    PHP Basic Challenge

--- 003.php
+++ 003.php
@@ -16,32 +16,51 @@ function GetDataBulan($data, $month)
 	return $result;
 }
 
-// var_dump(GetDataBulan($data, "February"))
 
 function GetDataByName($data, $name)
 {
+	$sum = 0;
 	for ($i=0; $i < count($data) ; $i++) { 
 		if (strtolower($data[$i]->customer->name) == strtolower($name))
 		{
 			$items = $data[$i]->items;
-			$sum = 0;
 			foreach ($items as $item) 
 			{
 				$sum += $item->price;
 			}
 
-			$result[] = (object)[
-				$data[$i],
-				'grand_total'=>$sum
+			// $result[] = (object)[
+			// 	$data[$i],
+			// 	'grand_total'=>$sum
 
-			];
+			// ];
+		}
+	}
+	return $sum;
+}
+
+function GetDataByGrandLow($data)
+{
+	for ($i=0; $i < count($data) ; $i++) { 
+		$items = $data[$i]->items;
+		$sum = 0;
+		foreach ($items as $item) 
+		{
+			$sum += $item->price;
+		}
+
+		if ($sum < 300000) 
+		{
+			$result[] = $data[$i]->customer->name;
 		}
 	}
 
 	return $result;
 }
 
-var_dump(GetDataByName($data, "Ari"))
+var_dump(GetDataBulan($data, "February"));
+var_dump(GetDataByName($data, "Ari"));
+print_r(array_unique(GetDataByGrandLow($data)));
 
 
 
